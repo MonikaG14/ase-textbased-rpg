8b986f4b2537f81e6bca89f40b248937b06b0c5d
MonikaG14 <85937393+MonikaG14@users.noreply.github.com>
2023-03-07 08:38:17 +0100

    Delete 2-application-ase-project/src/main/java/ase/project/application directory
    deletes unused directory

--- 2-application-ase-project/src/main/java/ase/project/application/exception/InvalidAttackException.java
+++
@@ -1,7 +0,0 @@
-package ase.project.application.exception;
-
-public class InvalidAttackException extends Throwable {
-    public InvalidAttackException(String message) {
-        super(message);
-    }
-}
