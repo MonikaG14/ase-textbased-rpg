0a133e99da542ba46f60481fe04557cb42a43566
MonikaG14 <monikarg95@gmail.com>
2023-03-16 14:49:13 +0100

    removes unsed imports

--- domain-ase-project-3/src/main/java/ase/project/domain/action/attack/BasicAttack.java
+++ domain-ase-project-3/src/main/java/ase/project/domain/action/attack/BasicAttack.java
@@ -1,7 +1,6 @@
 package ase.project.domain.action.attack;
 
 import ase.project.domain.characters.Character;
-import ase.project.domain.characters.Enemy;
 
 public interface BasicAttack {
     void useBasicAttack(Character target);
