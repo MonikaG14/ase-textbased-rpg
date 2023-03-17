7437da9109e7e5e5f56fe6cfd643e10e110802f3
MonikaG14 <monikarg95@gmail.com>
2023-03-17 16:48:02 +0100

    removes unused imports

--- domain-ase-project-3/src/main/java/ase/project/domain/characters/Enemy.java
+++ domain-ase-project-3/src/main/java/ase/project/domain/characters/Enemy.java
@@ -1,9 +1,5 @@
 package ase.project.domain.characters;
 
-import ase.project.domain.dice.DiceRoller;
-
-import static ase.project.domain.dice.DiceRoller.rollDice;
-
 public abstract class Enemy extends Character {
     protected int power;
 
