8e3ea52ce954b79247aa2baef7f9b82b96349f65
MonikaG14 <monikarg95@gmail.com>
2023-03-18 15:16:21 +0100

    testing the use of special attacks and exception throwing

--- application-ase-project-2/src/test/java/ase/project/application/player/AstronomerTest.java
+++ application-ase-project-2/src/test/java/ase/project/application/player/AstronomerTest.java
@@ -1,5 +1,8 @@
 package ase.project.application.player;
 
+import ase.project.application.enemies.PhyrexianMite;
+import ase.project.application.exception.InvalidManaException;
+import ase.project.domain.action.attack.SpecialAttack;
 import ase.project.domain.characters.Enemy;
 import ase.project.domain.dice.DiceRoller;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,36 +12,112 @@ import org.mockito.Mock;
 import org.mockito.MockedStatic;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class AstronomerTest {
     private Astronomer player;
+    private Astronomer playerWithNoMana;
+    private Enemy target;
 
     @Mock
-    Enemy target;
+    Enemy mockTarget;
 
     @BeforeEach
     public void setUp() {
         player = new Astronomer("AstroTest", 3, 3, 60, 60, 40, 10);
+        playerWithNoMana = new Astronomer("No Mana Astro", 3, 3, 40, 60, 1, 10);
+        target = new PhyrexianMite("Test Mite", 25, 30, 10, null);
     }
 
     @Test
-    void useBasicAttack() {
+    void testUseBasicAttack() {
         final int diceRoll = 4;
 
         try (MockedStatic<DiceRoller> diceRollerMock = mockStatic(DiceRoller.class)) {
             diceRollerMock.when(() -> DiceRoller.rollDice(6)).thenReturn(diceRoll);
-
             int expectedDamage = diceRoll + player.getIntelligence();
 
             player.useBasicAttack(target);
+            player.useBasicAttack(mockTarget);
 
-            verify(target).takeDamage(expectedDamage);
+            assertEquals(11, target.getCurrentHealth());
+            verify(mockTarget).takeDamage(expectedDamage);
         }
     }
 
     @Test
-    void useSpecialAttack() {
+    void testSpecialAttackListInitialization() {
+        Map<String, SpecialAttack> specialAttackMap = player.getSpecialAttackList();
+
+        assertEquals(2, specialAttackMap.size());
+        assertTrue(specialAttackMap.containsKey("Fireball Barrage"));
+        assertTrue(specialAttackMap.containsKey("Flame Tsunami"));
     }
+
+    @Test
+    void testUseSpecialAttack_performsCorrectAttack_FireballBarrage() {
+        String attackName = "Fireball Barrage";
+        int diceRoll = 20;
+
+        try (MockedStatic<DiceRoller> diceRollerMock = mockStatic(DiceRoller.class)) {
+            diceRollerMock.when(() -> DiceRoller.rollDice(20)).thenReturn(diceRoll);
+
+            int expectedHealth = target.getCurrentHealth() - diceRoll;
+
+            player.useSpecialAttack(target, attackName);
+            player.useSpecialAttack(mockTarget, attackName);
+
+            assertEquals(expectedHealth, target.getCurrentHealth());
+            verify(mockTarget).takeDamage(diceRoll);
+        }
+    }
+
+    @Test
+    void testUseSpecialAttack_performsCorrectAttack_FlameTsunami() {
+        String attackName = "Flame Tsunami";
+        int diceRoll = 20;
+
+        try (MockedStatic<DiceRoller> diceRollerMock = mockStatic(DiceRoller.class)) {
+            diceRollerMock.when(() -> DiceRoller.rollDice(20)).thenReturn(diceRoll);
+
+            int expectedHealth = target.getCurrentHealth() - diceRoll;
+
+            player.useSpecialAttack(target, attackName);
+            player.useSpecialAttack(mockTarget, attackName);
+
+            assertEquals(expectedHealth, target.getCurrentHealth());
+            verify(mockTarget).takeDamage(diceRoll);
+        }
+    }
+    @Test
+    void testUseSpecialAttack_reducesManaByCorrectAmount_FireballBarrage() {
+        String attackName = "Fireball Barrage";
+
+        player.useSpecialAttack(mockTarget, attackName);
+
+        assertEquals(35, player.getMana());
+    }
+
+    @Test
+    void testUseSpecialAttack_reducesManaByCorrectAmount_FlameTsunami() {
+        String attackName = "Flame Tsunami";
+
+        player.useSpecialAttack(mockTarget, attackName);
+
+        assertEquals(30, player.getMana());
+    }
+
+    /*@Test
+    void testUseSpecialAttack_throwsInvalidManaException() {
+        String attackName = "Fireball Barrage";
+
+        assertThrows(InvalidManaException.class, () -> {
+            playerWithNoMana.useSpecialAttack(mockTarget, attackName);
+        });
+    }
+     */
 }
