ebd8b89469c89d53538e608e2fa09576ef3cc449
MonikaG14 <monikarg95@gmail.com>
2023-03-23 12:53:53 +0100

    adds amount of potions for the player

--- domain-ase-project-3/src/main/java/ase/project/domain/characters/Player.java
+++ domain-ase-project-3/src/main/java/ase/project/domain/characters/Player.java
@@ -2,10 +2,14 @@ package ase.project.domain.characters;
 
 public abstract class Player extends Character {
     protected int mana;
+    protected int amountHealthPotions;
+    protected int amountManaPotions;
 
-    public Player(String name, int currentHealth, int maxHealth, int mana) {
+    public Player(String name, int currentHealth, int maxHealth, int mana, int amountHealthPotions, int amountManaPotions) {
         super(name, currentHealth, maxHealth);
         this.mana = mana;
+        this.amountHealthPotions = amountHealthPotions;
+        this.amountManaPotions = amountManaPotions;
     }
 
     public int takeDamage(int damage) {
@@ -19,4 +23,5 @@ public abstract class Player extends Character {
     public int getMana() {
         return mana;
     }
+
 }
