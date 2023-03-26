b6bb79897799491d86e19066307db9afbc93c227
MonikaG14 <monikarg95@gmail.com>
2023-03-26 15:49:49 +0200

    implements usePotion()

--- application-ase-project-2/src/main/java/ase/project/application/action/PotionService.java
+++ application-ase-project-2/src/main/java/ase/project/application/action/PotionService.java
@@ -2,16 +2,30 @@ package ase.project.application.action;
 
 import ase.project.application.item.potion.PotionFactory;
 import ase.project.application.item.potion.PotionType;
+import ase.project.application.player.PlayerClass;
+import ase.project.domain.characters.Player;
 import ase.project.domain.item.Potion;
 
 public class PotionService {
-    public static Potion choosePotion(PotionType potionType) {
+
+    public static Potion choosePotionType(PotionType potionType) {
         return PotionFactory.getPotion(potionType);
     }
 
-    public static void checkAmountOfPotions(int amount) {
-        if (amount == 0) {
+    public static void checkAmountOfPotions(Integer amount) {
+        if (amount == null) {
             throw new IllegalArgumentException("No potions left!");
         }
+    }
+
+    public static Player usePotion(String potionType, PlayerClass player) {
+        PotionType type = PotionType.valueOf(potionType.toUpperCase());
+        Potion potion = choosePotionType(type);
+
+        checkAmountOfPotions(player.getAmountOfPotionType().get(type));
+
+        player.updateAmountOfPotionType(type, player);
+
+        return potion.drink(player);
     }
 }
