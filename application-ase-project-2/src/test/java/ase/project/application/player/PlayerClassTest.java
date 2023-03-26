2faf6ccfb1dfb3ce3f0a6f1a6d821ef531172d89
MonikaG14 <monikarg95@gmail.com>
2023-03-26 15:52:40 +0200

    tests the update of the amount of certain potion type after drinking

---
+++ application-ase-project-2/src/test/java/ase/project/application/player/PlayerClassTest.java
@@ -0,0 +1,28 @@
+package ase.project.application.player;
+
+import ase.project.application.item.potion.PotionType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerClassTest {
+
+    @Mock
+    PlayerClass player;
+
+    @BeforeEach
+    public void setUp() {
+        player = new Astronomer("AstroTest", 60, 60, 40, 3, 2, 10);
+    }
+
+    @Test
+    void updateAmountOfPotionType() {
+        player.setPotionMaps();
+
+        player.updateAmountOfPotionType(PotionType.HEALTH, player);
+
+       assertEquals(2, player.amountOfPotionType.get(PotionType.HEALTH));
+    }
+}
