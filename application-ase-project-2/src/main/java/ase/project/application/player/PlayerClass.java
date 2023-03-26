8bd6c297f01af636f346ffe4d4613003f1014cd7
MonikaG14 <monikarg95@gmail.com>
2023-03-26 15:46:02 +0200

    updates the amount of potions after use

--- application-ase-project-2/src/main/java/ase/project/application/player/PlayerClass.java
+++ application-ase-project-2/src/main/java/ase/project/application/player/PlayerClass.java
@@ -1,7 +1,8 @@
 package ase.project.application.player;
 
 import ase.project.application.action.ManaService;
-import ase.project.application.action.attacks.ChooseSpecialAttack;
+import ase.project.application.action.PotionService;
+import ase.project.application.action.SpecialAttackService;
 import ase.project.application.exception.InvalidAttackException;
 import ase.project.application.exception.InvalidManaException;
 import ase.project.application.item.potion.PotionType;
@@ -14,18 +15,18 @@ import java.util.Map;
 
 public abstract class PlayerClass extends Player {
     protected Map<String, SpecialAttack> specialAttackList;
-    protected Map<PotionType, Integer> potionTypeAmount;
+    protected Map<PotionType, Integer> amountOfPotionType;
 
     public PlayerClass(String name, int currentHealth, int maxHealth, int mana, int amountHealthPotions, int amountManaPotions) {
         super(name, currentHealth, maxHealth, mana, amountHealthPotions, amountManaPotions);
         this.specialAttackList = new HashMap<>();
-        this.potionTypeAmount = new HashMap<>();
-        this.potionTypeAmount.put(PotionType.HEALTH, amountHealthPotions);
-        this.potionTypeAmount.put(PotionType.MANA, amountManaPotions);
+        this.amountOfPotionType = new HashMap<>();
+        amountOfPotionType.put(PotionType.HEALTH, amountHealthPotions);
+        amountOfPotionType.put(PotionType.MANA, amountManaPotions);
     }
 
     public void useSpecialAttack(Character target, String attackName) throws InvalidAttackException {
-        SpecialAttack specialAttack = ChooseSpecialAttack.chooseSpecialAttack(specialAttackList, attackName);
+        SpecialAttack specialAttack = SpecialAttackService.chooseSpecialAttack(specialAttackList, attackName);
         try {
             ManaService.checkMana(mana, specialAttack.getManaCost());
             specialAttack.performSpecialAttack(target, attackName);
@@ -35,11 +36,25 @@ public abstract class PlayerClass extends Player {
         }
     }
 
+    public Player updateAmountOfPotionType(PotionType potionType, PlayerClass player) {
+        this.amountOfPotionType.put(potionType, player.amountOfPotionType.get(potionType) - 1);
+        return this;
+    }
+
+    public Player drink(String potionType, PlayerClass player) {
+        return PotionService.usePotion(potionType, player);
+    }
+
+    public void setPotionMaps() {
+        amountOfPotionType.put(PotionType.HEALTH, amountHealthPotions);
+        amountOfPotionType.put(PotionType.MANA, amountManaPotions);
+    }
+
     public Map<String, SpecialAttack> getSpecialAttackList() {
         return this.specialAttackList;
     }
 
-    public Map<PotionType, Integer> getPotionTypeAmount() {
-        return potionTypeAmount;
+    public Map<PotionType, Integer> getAmountOfPotionType() {
+        return amountOfPotionType;
     }
 }
