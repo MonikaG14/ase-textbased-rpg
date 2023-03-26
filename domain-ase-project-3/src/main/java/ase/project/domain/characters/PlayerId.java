f4520e06f7a1b828c9e256ba880c49e1772b6a3b
MonikaG14 <monikarg95@gmail.com>
2023-03-26 17:21:58 +0200

    implements PlayerId as a Value Object

---
+++ domain-ase-project-3/src/main/java/ase/project/domain/characters/PlayerId.java
@@ -0,0 +1,34 @@
+package ase.project.domain.characters;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class PlayerId {
+    private final int id;
+    public PlayerId() {
+        this.id = getId();
+    }
+
+    public int getId() {
+        UUID uuid = UUID.randomUUID();
+        long uuidMostSigBits = uuid.getMostSignificantBits();
+        long uuidLeastSigBits = uuid.getLeastSignificantBits();
+
+        long longId = (uuidMostSigBits & 0xFFFFFFFFL) << 32 | (uuidLeastSigBits & 0xFFFFFFFFL);
+        int intId = (int) (longId & 0xFFFFFFFFL);
+        return Math.abs(intId);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this ) return true;
+        if (!(object instanceof PlayerId)) return false;
+        PlayerId other = (PlayerId) object;
+        return Objects.equals(id, other.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
