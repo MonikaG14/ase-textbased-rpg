400f11b4640894b5e260da302ab966916b903759
MonikaG14 <monikarg95@gmail.com>
2023-03-26 15:51:47 +0200

    tests drink() from manaPotion

---
+++ application-ase-project-2/src/test/java/ase/project/application/item/potion/ManaPotionTest.java
@@ -0,0 +1,33 @@
+package ase.project.application.item.potion;
+
+import ase.project.application.player.Astronomer;
+import ase.project.application.player.PlayerClass;
+import ase.project.domain.characters.Player;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManaPotionTest {
+
+    @Mock
+    PlayerClass mockPlayer;
+
+    @BeforeEach
+    public void setMockPlayer() {
+        mockPlayer = new Astronomer("Test", 60, 60, 30, 3, 2, 10);
+    }
+
+    @Test
+    void drink() {
+        ManaPotion manaPotion = new ManaPotion();
+
+        Player result = manaPotion.drink(mockPlayer);
+
+        assertEquals(mockPlayer, result);
+        assertEquals(1, result.getAmountManaPotions());
+        assertEquals(40, result.getMana());
+    }
+
+}
