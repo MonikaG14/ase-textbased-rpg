70ac0b947bfa273f2b4a4c78532975554c170036
MonikaG14 <monikarg95@gmail.com>
2023-04-02 14:32:02 +0200

    adds special attack Infest for the Phyrexian Mite

---
+++ application-ase-project-2/src/main/java/ase/project/application/action/specialattack/enemy/Infest.java
@@ -0,0 +1,25 @@
+package ase.project.application.action.specialattack.enemy;
+
+import ase.project.domain.action.attack.SpecialAttack;
+import ase.project.domain.characters.Character;
+import ase.project.domain.dice.DiceRoller;
+
+public class Infest implements SpecialAttack {
+    @Override
+    public void performSpecialAttack(Character target) {
+        int damage = DiceRoller.rollDice(20);
+        target.takeDamage(damage);
+        System.out.println(this.getDescription());
+        System.out.println("You took " + damage + " damage.");
+    }
+
+    @Override
+    public int getManaCost() {
+        return 5;
+    }
+
+    @Override
+    public String getDescription() {
+        return "When the Phyrexian Mite unleashes its Infest attack. \nSuddenly, you start feeling itchy and begin to scratch yourself uncontrollably.";
+    }
+}
