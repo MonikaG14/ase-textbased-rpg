928a834fa46de0f24d75dcc79e41e6a286c4315f
MonikaG14 <monikarg95@gmail.com>
2023-04-17 17:58:15 +0200

    refactoring - following the Open-Closed-Principle: ChooseClass now takes objects from PlayerClassFactory and is easier to maintain when new player classes are added

---
+++ adapters-ase-project-1/src/main/java/ase/project/adapters/player/ChooseClass.java
@@ -0,0 +1,41 @@
+package ase.project.adapters.player;
+
+import ase.project.domain.action.InputProvider;
+import ase.project.domain.characters.Player;
+
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class ChooseClass {
+
+    private final InputProvider inputProvider;
+    private final PlayerClassFactory playerClassFactory = new PlayerClassFactory();
+
+    Map<Integer, Supplier<Player>> classes = playerClassFactory.getCLASS_MAP();
+
+    public ChooseClass(InputProvider inputProvider) {
+        this.inputProvider = inputProvider;
+    }
+
+    public Player chooseClass() {
+        this.iterateClasses();
+        int classKey = inputProvider.readInt();
+
+        try {
+            Player player = playerClassFactory.createPlayerClass(classKey);
+            System.out.println("You chose the class " + player.getClass().getSimpleName());
+            return player;
+        } catch (IllegalArgumentException illegalArgumentException) {
+            System.out.println("Invalid choice. Try again.");
+            return chooseClass();
+        }
+    }
+
+    public void iterateClasses() {
+        System.out.println("Choose your class: ");
+        Map<Integer, Supplier<Player>> classes = playerClassFactory.getCLASS_MAP();
+        for (Map.Entry<Integer, Supplier<Player>> entry : classes.entrySet()) {
+            System.out.println(entry.getKey() + ". " + entry.getValue().get().getName());
+        }
+    }
+}
--- adapters-ase-project-1/src/test/java/ase/project/adapters/ChooseClassTest.java
+++ adapters-ase-project-1/src/test/java/ase/project/adapters/ChooseClassTest.java
@@ -1,11 +1,11 @@
 package ase.project.adapters;
 
+import ase.project.adapters.player.ChooseClass;
 import ase.project.application.player.classes.Astronomer;
+import ase.project.domain.action.InputProvider;
 import ase.project.domain.characters.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
-import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -17,12 +17,12 @@ class ChooseClassTest {
 
     @BeforeEach
     void setUp() {
-        Scanner scanner = mock(Scanner.class);
-        when(scanner.nextInt()).thenReturn(1);
-        chooseClass = new ChooseClass(scanner);
+        InputProvider inputProvider = mock(InputProvider.class);
+        when(inputProvider.readInt()).thenReturn(1);
+        chooseClass = new ChooseClass(inputProvider);
     }
     @Test
-    void testAddition() {
+    void testChooseClass() {
         Player player = chooseClass.chooseClass();
         assertTrue(player instanceof Astronomer);
     }
