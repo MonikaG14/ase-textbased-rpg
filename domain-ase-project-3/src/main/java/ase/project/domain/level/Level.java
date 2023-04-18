813479e2255217baa441f9b42b39e04b88a92f8f
MonikaG14 <monikarg95@gmail.com>
2023-04-18 19:27:12 +0200

    enemies are now presented as Map instead of List

--- domain-ase-project-3/src/main/java/ase/project/domain/level/Level.java
+++ domain-ase-project-3/src/main/java/ase/project/domain/level/Level.java
@@ -4,20 +4,22 @@ import ase.project.domain.characters.Enemy;
 import ase.project.domain.item.Item;
 
 import java.util.List;
+import java.util.Map;
 
 public abstract class Level {
-    protected List<Enemy> enemies;
+
+    protected Map<Integer, Enemy> enemies;
     protected List<Item> items;
     protected String startingLevelDescription;
     protected String endingLevelDescription;
 
-    public Level(List<Enemy> enemies, List<Item> items, String startingLevelDescription) {
+    public Level(Map<Integer, Enemy> enemies, List<Item> items, String startingLevelDescription) {
         this.enemies = enemies;
         this.items = items;
         this.startingLevelDescription = startingLevelDescription;
     }
 
-    public List<Enemy> getEnemies() {
+    public Map<Integer, Enemy> getEnemies() {
         return enemies;
     }
 
