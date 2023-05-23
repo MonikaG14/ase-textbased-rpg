960c485882acd18c8b24a6cf853b1b3f6ba8cee5
MonikaG14 <monikarg95@gmail.com>
2023-05-23 11:30:04 +0200

    Test methods for the PlayerStatsRepository

---
+++ plugin-ase-project-0/src/test/java/ase/project/plugin/PlayerStatsRepositoryImplTest.java
@@ -0,0 +1,43 @@
+package ase.project.plugin;
+
+import ase.project.application.player.classes.Busker;
+import ase.project.domain.characters.player.Player;
+import ase.project.domain.characters.player.PlayerStats;
+import ase.project.domain.characters.player.PlayerStatsRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerStatsRepositoryImplTest {
+
+    private static final String FILENAME = "playerStats.txt";
+    private PlayerStatsRepository playerStatsRepository;
+
+    @BeforeEach
+    void setUp() {
+        playerStatsRepository = new PlayerStatsRepositoryImpl();
+    }
+
+    @Test
+    void testSavePlayerStats() {
+        PlayerStats playerStats = new PlayerStats(1, "Player1", 100, 200, 50, 3, 2);
+
+        playerStatsRepository.savePlayerStats(playerStats);
+
+        File file = new File(FILENAME);
+        assertTrue(file.exists());
+    }
+
+    @Test
+    void testGetPlayerStats() {
+        Player player = new Busker("Player1", 100, 200, 50, 3, 2, 10);
+
+        playerStatsRepository.getPlayerStats(player);
+
+        File file = new File(FILENAME);
+        assertTrue(file.exists());
+    }
+}
