ca8dc25effc1d535cd82b7247b5932f9d4708c64
MonikaG14 <monikarg95@gmail.com>
2023-05-23 10:37:07 +0200

    add EnemyCombatManager

---
+++ adapters-ase-project-1/src/main/java/ase/project/adapters/EnemyCombatManager.java
@@ -0,0 +1,20 @@
+package ase.project.adapters;
+
+import ase.project.application.enemies.EnemyManager;
+import ase.project.application.exception.InvalidAttackException;
+import ase.project.domain.characters.player.Player;
+import ase.project.domain.level.Level;
+
+public class EnemyCombatManager {
+
+    private final DeathObserverManager deathObserverManager;
+
+    public EnemyCombatManager(DeathObserverManager deathObserverManager) {
+        this.deathObserverManager = deathObserverManager;
+    }
+
+    public void chooseAction(EnemyManager enemy, Player player, Level level) throws InvalidAttackException {
+        enemy.chooseBetweenSpecialAndBasicAttack(player);
+        deathObserverManager.checkIfDead(player, level);
+    }
+}
