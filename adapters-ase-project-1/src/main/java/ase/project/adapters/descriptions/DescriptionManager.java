3fe9f646c141f69363790937b87586a8a4660a7a
MonikaG14 <monikarg95@gmail.com>
2023-05-26 15:12:25 +0200

    add missing attribute, methods and constructors to the UML diagrams

--- adapters-ase-project-1/src/main/java/ase/project/adapters/descriptions/DescriptionManager.java
+++ adapters-ase-project-1/src/main/java/ase/project/adapters/descriptions/DescriptionManager.java
@@ -1,6 +1,7 @@
 package ase.project.adapters.descriptions;
 
 import ase.project.domain.action.DescriptionLoader;
+import ase.project.domain.action.InputProvider;
 import ase.project.domain.characters.enemy.Enemy;
 import ase.project.domain.level.Level;
 
@@ -9,16 +10,58 @@ import java.util.*;
 public class DescriptionManager {
 
     private final DescriptionLoader descriptionLoader;
+    private final InputProvider inputProvider;
 
-    public DescriptionManager(DescriptionLoader descriptionLoader) {
+    public DescriptionManager(DescriptionLoader descriptionLoader, InputProvider inputProvider) {
         this.descriptionLoader = descriptionLoader;
+        this.inputProvider = inputProvider;
+    }
+
+    public void printStartingGame() {
+        System.out.println(formatDescription(descriptionLoader.getDescription("starting1")));
+        pressContinue();
+        System.out.println(formatDescription(descriptionLoader.getDescription("starting2")));
+        pressContinue();
+        System.out.println(formatDescription(descriptionLoader.getDescription("starting3")));
+        pressContinue();
+        System.out.println(formatDescription(descriptionLoader.getDescription("starting4")));
+        pressContinue();
+    }
+
+    public void printClassDescriptions() {
+        System.out.println(formatDescription(descriptionLoader.getDescription("choose")));
+        System.out.println();
+        System.out.println(formatDescription(descriptionLoader.getDescription("astronomer")));
+        pressContinue();
+        System.out.println(formatDescription(descriptionLoader.getDescription("busker")));
+        pressContinue();
+        System.out.println(formatDescription(descriptionLoader.getDescription("gladiator")));
+        pressContinue();
     }
 
     public String getWeaponDescription(String weaponKey) {
         return formatDescription(descriptionLoader.getDescription(weaponKey));
     }
 
+    public void printPlayerWeaponDescription(String playerClass) {
+        Map<String, String> playerWeaponMapping = new HashMap<>();
+        playerWeaponMapping.put("Gladiator", "cursed_longsword");
+        playerWeaponMapping.put("Busker", "the_lute-nuke");
+        playerWeaponMapping.put("Astronomer", "makeshift_staff");
+
+        String weaponKey = playerWeaponMapping.get(playerClass);
+        if (weaponKey == null) {
+            throw new IllegalArgumentException("Invalid player class: " + playerClass);
+        }
+
+        String weaponDescription = getWeaponDescription(weaponKey);
+        System.out.println(formatDescription(weaponDescription));
+        pressContinue();
+    }
+
     public String getEnemyDescription(String enemyKey) {
+        System.out.println();
+        pressContinue();
         return formatDescription(descriptionLoader.getDescription(enemyKey));
     }
 
@@ -42,79 +85,62 @@ public class DescriptionManager {
         System.out.println();
     }
 
-    public void printPlayerWeaponDescription(String playerClass) {
-        Map<String, String> playerWeaponMapping = new HashMap<>();
-        playerWeaponMapping.put("Gladiator", "cursed_longsword");
-        playerWeaponMapping.put("Busker", "the_lute-nuke");
-        playerWeaponMapping.put("Astronomer", "makeshift_staff");
+    public void printFirstLevelDescription() {
+        System.out.println(formatDescription(descriptionLoader.getDescription("first_level1")));
+        System.out.println(formatDescription(descriptionLoader.getDescription("first_level2")));
+        System.out.println(formatDescription(descriptionLoader.getDescription("creatures")));
+    }
 
-        String weaponKey = playerWeaponMapping.get(playerClass);
-        if (weaponKey == null) {
-            throw new IllegalArgumentException("Invalid player class: " + playerClass);
-        }
 
-        String weaponDescription = getWeaponDescription(weaponKey);
-        System.out.println(formatDescription(weaponDescription));
-        System.out.println();
+    public void printEndFirstLevel() {
+        System.out.println(formatDescription(descriptionLoader.getDescription("end_of_first_level")));
+        pressContinue();
     }
 
     public void printLevelDescription(Level level) {
         System.out.println(formatDescription(level.getStartingLevelDescription()));
-        System.out.println();
-    }
-
-    public void printFirstLevelDescription() {
-        System.out.println(formatDescription(descriptionLoader.getDescription("first_level")));
+        System.out.println(formatDescription(descriptionLoader.getDescription("creatures")));
+        pressContinue();
     }
 
     public void printFinalLevelDescription() {
         System.out.println(formatDescription(descriptionLoader.getDescription("final_level")));
-        System.out.println();
+        pressContinue();
     }
 
     public void printEndingGame() {
         System.out.println(formatDescription(descriptionLoader.getDescription("ending")));
-        System.out.println();
     }
 
-    public void printClassDescriptions() {
-        System.out.println(formatDescription(descriptionLoader.getDescription("astronomer")));
-        System.out.println();
-        System.out.println(formatDescription(descriptionLoader.getDescription("busker")));
-        System.out.println();
-        System.out.println(formatDescription(descriptionLoader.getDescription("gladiator")));
-        System.out.println();
+    public void pressContinue() {
+        System.out.println("Press any button to continue...");
+        inputProvider.readChar();
     }
+
 
     public String formatDescription(String description) {
-        // Remove leading and trailing whitespaces
         int maxWidth = 100;
         description = description.trim();
 
-        // Split the description into words
         String[] words = description.split("\\s+");
 
         StringBuilder formattedDescription = new StringBuilder();
         StringBuilder currentLine = new StringBuilder();
         int currentWidth = 0;
 
-        // Process each word
         for (String word : words) {
             int wordWidth = word.length() + 1; // Add 1 for the space after the word
 
             if (currentWidth + wordWidth <= maxWidth) {
-                // Add the word to the current line
                 currentLine.append(word).append(" ");
                 currentWidth += wordWidth;
             } else {
-                // Start a new line with the word
                 formattedDescription.append(currentLine).append(System.lineSeparator());
                 currentLine = new StringBuilder(word).append(" ");
                 currentWidth = wordWidth;
             }
         }
 
-        // Append the last line
         formattedDescription.append(currentLine);
 
         return formattedDescription.toString();
